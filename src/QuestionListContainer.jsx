6db6a7b34646be40df1031b2dd66a08454334723
jun <nr0307@amoeba.co.kr>
2020-01-06 16:12:20 +0900

    [update]메인으로 가는 버튼 추가

--- src/QuestionListContainer.jsx
+++ src/QuestionListContainer.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-const QuestionListContainer = ({ questionList = [], onClick, quizScores = {} }) => {
+const QuestionListContainer = ({ questionList = [], onClick, quizScores = {}, onClickMain }) => {
   return (
-    <div className="items">
-      { questionList.map( (item, index) => {
-        return (
-          <div className="item" key={ item.name }>
-            <h3>{ item.name }</h3>
-            <p>점수 : { quizScores[ index ] ? quizScores[ index ] : 0 }점</p>
-            <button className="btn" onClick={ onClick } data-order={ index }>시작</button>
-          </div>
-        );
-      } ) }
-    </div>
+    <>
+      <div className="items">
+        { questionList.map( (item, index) => {
+          return (
+            <div className="item" key={ item.name }>
+              <h3>{ item.name }</h3>
+              <p>점수 : { quizScores[ index ] ? quizScores[ index ] : 0 }점</p>
+              <button className="btn" onClick={ onClick } data-order={ index }>시작</button>
+            </div>
+          );
+        } ) }
+      </div>
+      <div className="back">
+        <button className="btn btn-back" onClick={ onClickMain   }>처음으로</button>
+      </div>
+
+    </>
   );
 };
 
