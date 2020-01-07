ce85d7d6c3cbcf39d2d0be0626bf85242f162272
kdeveloper87 <k.developer87@gmail.com>
2020-01-07 14:21:33 +0900

    [update]component 분리

--- src/LevelContainer.jsx
+++ src/LevelContainer.jsx
@@ -3,16 +3,41 @@ import React from "react";
 const LevelContainer = ({ onClick }) => {
   return (
     <div className="contents">
-      <div className="card">
-        <h3>1단계 문제</h3>
-        <div className="main_text">
-          <p>자바스크립트 1단계 문제 <br/>문제를 보고 맞는 답을 골라주세요.</p>
+      <QuestionCard
+        title={ '기초문제' }
+        contents={ `기초입니다!. \n 문제를 보고 맞는 답을 골라주세요.` }
+        level={ LEVEL.INTERMEDIATE }
+        onClick={ onClick }
+      />
 
-        </div>
-        <button className="btn" onClick={ onClick } data-level={ 'advanced' }>시작</button>
-      </div>
+      <QuestionCard
+        title={ '중급문제' }
+        contents={ '조금 어려운 중급문제입니다. \n 문제를 보고 맞는 답을 골라주세요.' }
+        level={ LEVEL.ADVANCED }
+        onClick={ onClick }
+      />
     </div>
   );
+};
+
+const QuestionCard = ({ title, contents, onClick, level }) => {
+  return (
+    <div className="card">
+      <h3>{ title }</h3>
+      <div className="main_text">
+        <p>{ contents.split( '\n' ).map( content => {
+          return ( <span>{ content }<br/></span> )
+        } ) }
+        </p>
+      </div>
+      <button className="btn" onClick={ onClick } data-level={ level }>시작</button>
+    </div>
+  );
+};
+
+const LEVEL = {
+  INTERMEDIATE: 'intermediate',
+  ADVANCED: 'advanced',
 };
 
 export default LevelContainer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
-import React from  'react' ;
-import ReactDom from  'react-dom' ;
-import { hot } from  'react-hot-loader/root' ;
+import React from 'react' ;
+import ReactDom from 'react-dom' ;
+import { hot } from 'react-hot-loader/root' ;
 
 import App from './App' ;
 
 const Hot = hot( App );
 
 ReactDom.render( <Hot/>, document.querySelector( '#root' ) );
+
