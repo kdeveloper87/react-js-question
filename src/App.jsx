c2abf81c6eaa08c9b2dd5e798970d685a6f143f6
kdeveloper87 <k.developer87@gmail.com>
2020-01-19 15:45:28 +0900

    [update]state->redux state 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import React from 'react' ;
 import './css/reset.css';
 import './css/style.css'
 import MainContainer from "./containers/MainContainer";
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore( rootReducer, composeWithDevTools() );
 
 const App = () => {
   return (
-    <MainContainer/>
+    <Provider store={ store }>
+      <MainContainer/>
+    </Provider>
   );
 };
 
