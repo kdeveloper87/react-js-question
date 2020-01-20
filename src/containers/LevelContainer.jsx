e575b73dc260a0f20a480a2698901170eae49a72
JunH-K <k.developer@kakao.com>
2020-01-20 23:37:29 +0900

    [update]text 분리

--- src/containers/LevelContainer.jsx
+++ src/containers/LevelContainer.jsx
@@ -4,15 +4,15 @@ const LevelContainer = ({ onClick }) => {
   return (
     <div className="contents">
       <QuestionCard
-        title={ '기초문제' }
-        contents={ `기초입니다!. \n 문제를 보고 맞는 답을 골라주세요.` }
+        title={ titleLevel1 }
+        contents={ contentsLevel1 }
         level={ LEVEL.INTERMEDIATE }
         onClick={ onClick }
       />
 
       <QuestionCard
-        title={ '중급문제' }
-        contents={ '조금 어려운 중급문제입니다. \n 문제를 보고 맞는 답을 골라주세요.' }
+        title={ titleLevel2 }
+        contents={ contentsLevel2 }
         level={ LEVEL.ADVANCED }
         onClick={ onClick }
       />
@@ -25,8 +25,8 @@ const QuestionCard = ({ title, contents, onClick, level }) => {
     <div className="card">
       <h3>{ title }</h3>
       <div className="main_text">
-        <p>{ contents.split( '\n' ).map( (content,index) => {
-          return ( <span key={index}>{ content }<br/></span> )
+        <p>{ contents.split( '\n' ).map( (content, index) => {
+          return (<span key={ index }>{ content }<br/></span>)
         } ) }
         </p>
       </div>
@@ -39,5 +39,10 @@ const LEVEL = {
   INTERMEDIATE: 'intermediate',
   ADVANCED: 'advanced',
 };
+
+const titleLevel1 = '기초문제';
+const titleLevel2 = '기초입니다!. \n 문제를 보고 맞는 답을 골라주세요.';
+const contentsLevel1 = '중급문제';
+const contentsLevel2 = '조금 어려운 중급문제입니다. \n 문제를 보고 맞는 답을 골라주세요.';
 
 export default LevelContainer;
