7715402e88e78be3ab42755106d5cd98ea59272b
JunH-K <k.developer@kakao.com>
2020-02-13 10:56:09 +0900

    style : prettier

--- src/containers/QuestionContainer.jsx
+++ src/containers/QuestionContainer.jsx
@@ -86,15 +86,18 @@ const QuestionContainer = ({ match, history }) => {
     setCurQuestion(data[nextIndex]);
   }, [curIndex]);
 
-  const updateScore = useCallback(score => {
-    let nextScores = { ...scores };
-    if (!scores[level] || index === null) {
-      nextScores[level] = [];
-    }
-    nextScores[level][index] = score;
-    dispatch(scoresAction({ scores: nextScores }));
-    history.push(`/level/${level}`);
-  },[scores]);
+  const updateScore = useCallback(
+    score => {
+      let nextScores = { ...scores };
+      if (!scores[level] || index === null) {
+        nextScores[level] = [];
+      }
+      nextScores[level][index] = score;
+      dispatch(scoresAction({ scores: nextScores }));
+      history.push(`/level/${level}`);
+    },
+    [scores]
+  );
 
   return (
     <div className="question_container">
